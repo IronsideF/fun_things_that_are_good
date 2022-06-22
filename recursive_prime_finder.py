c36d130c47c8b1a5e43c87f67c4d886dcec3c5a4
Nathaniel Forsyth <ironsideforsyth@gmail.com>
2022-06-22 15:08:19 +0100

    initial commit

---
+++ recursive_prime_finder.py
@@ -0,0 +1,17 @@
+
+
+def prime_nums(initial, end):
+    testing_range = list(range(initial, end))
+    prime_num_list = [2, ]
+    for num in testing_range:
+        is_prime = True
+        for i in prime_num_list:
+            if num % i == 0:
+                is_prime = False      
+        if is_prime == False:
+            pass
+        else:
+            prime_num_list.append(num)
+    print(prime_num_list)
+
+prime_nums(3, 101)
