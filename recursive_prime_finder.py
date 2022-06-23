5a5f53762162ef42c8f64836dc953d1a66931242
Nathaniel Forsyth <ironsideforsyth@gmail.com>
2022-06-23 09:58:16 +0100

    fixed an issue if 1 is in the range and prevented users from starting from anything but 1

--- recursive_prime_finder.py
+++ recursive_prime_finder.py
@@ -1,15 +1,17 @@
-def prime_nums(initial, end):
-    testing_range = range(initial, end)
+def prime_nums(end):
+    testing_range = range(1, end)
     prime_num_list = [2, ]
     for num in testing_range:
         is_prime = True
         for i in prime_num_list:
             if num % i == 0:
-                is_prime = False      
+                is_prime = False   
+            elif num == 1:
+                is_prime = False   
         if is_prime == False:
             pass
         else:
             prime_num_list.append(num)
     print(prime_num_list)
 
-prime_nums(3, 101)
+prime_nums(101)
